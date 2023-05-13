d38f7256f28d04969f0b8809c3c4098ad503e04e
Thiago Marinho <thiagoo_100@hotmail.com>
2023-05-13 00:12:45 -0300

    Adicionando Teste de unitario para o metodo generate_number

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.example.diceroller
 
-import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertTrue
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number(){
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("O resultado nao estava entre 1 e 6", rollResult in 1..6)
     }
 }
